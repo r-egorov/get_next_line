221f0d6462cd2afecc3119196a04fedf78cc6666
Chansey Isis <cisis@at-q5.msk.21-school.ru>
2020-11-15 13:57:53 +0300

    Refactored parenthesis: now works

--- get_next_line.c
+++ get_next_line.c
@@ -6,31 +6,29 @@
 /*   By: cisis <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/11 17:40:52 by cisis             #+#    #+#             */
-/*   Updated: 2020/11/14 18:48:40 by cisis            ###   ########.fr       */
+/*   Updated: 2020/11/15 13:57:12 by cisis            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-static int	get_line(char **lineptr, char **remainder)
+static int	get_line(char **line, char **remainder)
 {
 	char	*nwln;
-	char	*line;
 	char	*tmp;
 
-	line = *lineptr;
 	nwln = NULL;
 	if ((nwln = ft_strchr(*remainder, '\n')))
 	{
 		*nwln = '\0';
 		tmp = *remainder;
-		line = ft_strdup(*remainder);
+		*line = ft_strdup(*remainder);
 		*remainder = ft_strdup(++nwln);
 		free(tmp);
 	}
 	else
 	{
-		line = ft_strdup(*remainder);
+		*line = ft_strdup(*remainder);
 		free(*remainder);
 		return (0);
 	}
@@ -44,14 +42,14 @@ static int	check_remainder(char **line, char **remainder, int bytes_read)
 	return (get_line(line, remainder));
 }
 
-int		get_next_line(int fd, char **line)
+int			get_next_line(int fd, char **line)
 {
 	static char	*remainder;
 	char		*buf;
 	char		*tmpbuf;
-	int		bytes_read;
+	int			bytes_read;
 
-	if (BUFFER_SIZE < 1 || !(*line) || 
+	if (BUFFER_SIZE < 1 || !(*line) ||
 	!(buf = (char*)malloc((BUFFER_SIZE + 1) * sizeof(char))))
 		return (-1);
 	while ((bytes_read = read(fd, buf, BUFFER_SIZE) > 0))
@@ -66,7 +64,7 @@ int		get_next_line(int fd, char **line)
 			free(tmpbuf);
 		}
 		if (ft_strchr(remainder, '\n'))
-			break;
+			break ;
 	}
 	free(buf);
 	return (check_remainder(line, &remainder, bytes_read));
